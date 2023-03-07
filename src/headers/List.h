39cb41e158bd51fc3a944c4238f589b2dcf6470c
walig-here <daw.walig@gmail.com>
2023-03-07 15:54:07 +0100

    Rozpoczecie prac nad listą dwukierunkową

---
+++ src/headers/List.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/* Struktura reprezentująca element listy */
+struct ListElement{
+    int value;  // wartość zawarta w elemencie listy
+    int* next;  // adres kolejnego elementu
+    int* prev;  // adres poprzedniego elementu 
+};
+
+/* Klasa list dwukierunkowych */
+class List
+{
+
+    /* POLA */
+    
+    /* METODY */
+
+};
