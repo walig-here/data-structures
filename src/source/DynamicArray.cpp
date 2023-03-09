e6afbfdc9e0d0560b610a3f4d851e3f791cf4441
walig-here <daw.walig@gmail.com>
2023-03-09 11:39:20 +0100

    Dodanie klas tablicy dynamicznej i listy

---
+++ src/source/DynamicArray.cpp
@@ -0,0 +1,37 @@
+#include "DynamicArray.h"
+#include <iostream>
+
+using namespace std;
+
+DynamicArray::DynamicArray(){
+
+    array_address = nullptr;
+    length = 0;
+
+}
+
+DynamicArray::~DynamicArray(){
+
+    // Zwolnienie obaszaru pamięci zajmowanego przez tablice
+    if(array_address == nullptr) return;
+    delete[] array_address;
+    array_address = nullptr;
+
+}
+
+int DynamicArray::operator[](int index){
+
+    // Sprawdzenie poprawnosci indeksu
+    if(index < 0 || index >= length) return NULL;
+
+    // Zwrócenie wartości zawartej pod podanym indeksem
+    return array_address[index];
+
+}
+
+void DynamicArray::add(int new_element){
+
+    // Relokacja tablicy
+    array_address = (int*) realloc(array_address, ++length * sizeof(int));
+
+}
